c4bf0b2df9fc532d23bf41cf3ae5467e16b03b0e
Peter5793 <masaba.peter68@gmail.com>
2020-12-10 21:53:54 +0300

    Change indentation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
         print("1 argument:\n: {}".format(argv[1]))
     else:
         print("{} arguments:".format(x - 1))
-
-    for i in range(1, x):
-        print("{}: {}".format(i, argv[i]))
+        for i in range(1, x):
+            print("{}: {}".format(i, argv[i]))
