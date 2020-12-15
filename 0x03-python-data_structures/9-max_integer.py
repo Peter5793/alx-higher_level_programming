62c1fbc81e8116cefbb88c6a5cfa43374b00806e
Peter5793 <masaba.peter68@gmail.com>
2020-12-15 21:56:57 +0300

    Find the maximum number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    max_1 = 0
+    for i in my_list:
+        if i > max_1:
+            max_1 = i
+    return (max_1)
