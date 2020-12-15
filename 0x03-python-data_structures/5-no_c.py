5fd735c6d2e639fe7b885c8f65ff8f9ff86b0403
Peter5793 <masaba.peter68@gmail.com>
2020-12-15 20:15:26 +0300

    Remove c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None:
+        return None
+    dict = {ord('c'): None, ord('C'):None}
+    my_string_2 = my_string.translate(dict)
+    return my_string_2
