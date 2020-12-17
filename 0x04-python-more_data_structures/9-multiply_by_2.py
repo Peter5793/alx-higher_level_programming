8a1bb79a77951a470711d799e48c004b78db27e1
Peter5793 <masaba.peter68@gmail.com>
2020-12-17 21:49:32 +0300

    Multiply keys by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
-    return a_dictionary
+    n_dic = a_dictionary.copy()
+    for value in n_dic:
+        n_dic[value] *= 2
+    return n_dic
