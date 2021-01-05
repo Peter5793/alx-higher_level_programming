18bd8d7d48ce4a969d8ea40cb61ba7670372ec3f
Peter5793 <masaba.peter68@gmail.com>
2021-01-05 23:46:19 +0300

    Docstring correction

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""Define a square based on 2-square.py"""
+
+
 class Square:
+    """class square with private attribute instance, size"""
     def __init__(self, size=0):
+        """Initialize new square with size argument"""
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
 
     def area(self):
+        """Return area"""
         return self.__size * self.__size
