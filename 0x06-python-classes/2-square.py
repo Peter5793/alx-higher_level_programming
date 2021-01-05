2e61841e37781f437d0241cd12786bf6a28c8ec8
Peter5793 <masaba.peter68@gmail.com>
2021-01-05 23:31:44 +0300

    Fix docstring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""class square that defines a square by 1-square.py"""
+
+
 class Square:
+    """class Square with private instance atribute size"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        """initialization of new square with size argument"""
         self._size = size
