a0db5aed0df603547dc7b5d9c1bb28a4a21793f8
Peter5793 <masaba.peter68@gmail.com>
2021-01-19 22:19:12 +0300

    add create and saving objects function

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+import json module
+"""
+
+
+def load_from_json_file(filename):
+    """create object from a JSON file"""
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        return json.load(a_file)
