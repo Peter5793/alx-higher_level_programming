99c20dafe0363ec2249a2f06e271d221044d3413
Peter5793 <masaba.peter68@gmail.com>
2021-01-26 19:22:26 +0300

    Change error statements

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError('x must be an integer')
         elif value < 0:
-            raise ValueError('x must be > 0')
+            raise ValueError('x must be >= 0')
         else:
             self.__x = value
 
@@ -70,6 +70,6 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError('y must be an integer')
         elif value < 0:
-            raise ValueError('y must be > 0')
+            raise ValueError('y must be >= 0')
         else:
             self.__y = value
