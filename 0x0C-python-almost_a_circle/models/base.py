5755ccb6b663d5d9ad7c9c6f7c4891c05cf949aa
Peter5793 <masaba.peter68@gmail.com>
2021-01-27 17:17:34 +0300

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """
 Intialize a class named base
 """
+import json
+import csv
+
 
 
 class Base():
@@ -16,3 +19,16 @@ class Base():
         else:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string representation to list_objs to file"""
+        fname = cls.__name__+ ".json"
+        lista = []
+        with open(fname, mode='w', encoding='utf-8') as a_file:
+            if list_objs is None:
+                a_file.write(cls.to_json_string([]))
+            else:
+                for obj in list_objs:
+                    lista.append(obj.to_dictionary())
+                a_file.write(cls.to_json_string(lista))
