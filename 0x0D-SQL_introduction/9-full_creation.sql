c60736527da2f87ab54fcf69ba87ea1bf1511c67
Peter5793 <masaba.peter68@gmail.com>
2021-03-01 23:10:30 +0300

    add columns

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,8 @@
 -- Add records into the second table
-CREATE TABLE IF NOT EXISTS second_table;
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
 INSERT INTO second_table(`id`, `name`, `score`) VALUES(1, "John", 10),(2,
 	 "Alex", 3),(3, "Bob", 14), (3, "George", 8);
