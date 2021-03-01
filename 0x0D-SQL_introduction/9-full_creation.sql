6bae3ce76f7650b573aebe3bf00aeb8dfb49f2d9
Peter5793 <masaba.peter68@gmail.com>
2021-03-01 23:08:11 +0300

    add create script

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
 -- Add records into the second table
+CREATE TABLE IF NOT EXISTS second_table;
 INSERT INTO second_table(`id`, `name`, `score`) VALUES(1, "John", 10),(2,
 	 "Alex", 3),(3, "Bob", 14), (3, "George", 8);
