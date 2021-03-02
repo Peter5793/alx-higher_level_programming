2eac98a2ceaa7dcd325eeb51d950ae3725fe2970
Peter5793 <masaba.peter68@gmail.com>
2021-03-02 20:54:10 +0300

    make states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database hbnt_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256)
+	);
