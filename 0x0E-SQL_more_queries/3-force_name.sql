26e564131ab57334f17125458bb40f61b0df7281
Peter5793 <masaba.peter68@gmail.com>
2021-03-02 20:21:53 +0300

    add NULL

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,4 @@
 -- Create table force_name on the MySQL server
-CREATE TABLE IF NOT force_name EXIST force_name (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT force_name EXIST force_name 
+(id INT, 
+name VARCHAR(256) NOT NULL);
