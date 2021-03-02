b468301fb483e1e436fb9e2b1e3b7b00c35eda1e
Peter5793 <masaba.peter68@gmail.com>
2021-03-02 20:23:55 +0300

    Edit typo

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Create table force_name on the MySQL server
-CREATE TABLE IF NOT force_name EXIST force_name 
-(id INT, 
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT, 
+	name VARCHAR(256) NOT NULL);
