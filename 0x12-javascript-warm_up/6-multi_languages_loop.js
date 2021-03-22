a02b95f853541b29810686c7ba62b98c60ed8911
Peter5793 <masaba.peter68@gmail.com>
2021-03-22 21:57:22 +0300

    add iteration

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const prog = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
 
-for (const i in prog) {
+for (; i < prog.length; i++) {
   console.log(prog[i]);
 }
