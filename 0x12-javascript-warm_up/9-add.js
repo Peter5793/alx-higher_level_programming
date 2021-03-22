d47ce5c89e3abf1b2b7c1ab03606c1de5165aed0
Peter5793 <masaba.peter68@gmail.com>
2021-03-22 23:38:43 +0300

    check semistandard error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 const num1 = parseInt(argv[2]);
 const num2 = parseInt(argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   console.log(a + b);
 }
-add(num1 , num2);
+add(num1, num2);
