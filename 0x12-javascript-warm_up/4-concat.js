bde52b90de5e06624c7f9066db7d398da21553d3
Peter5793 <masaba.peter68@gmail.com>
2021-03-22 21:26:34 +0300

    concate in command line argument

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (process.argv.length < 3) {
-	console.log('No argument');
-} else if (process.argv.length > 3) {
-	console.log (argv[2] + ' is ' + argv[3]);
-}
+const nArgv = argv[2] + ' is ' + argv[3];
+
+console.log(nArgv);
