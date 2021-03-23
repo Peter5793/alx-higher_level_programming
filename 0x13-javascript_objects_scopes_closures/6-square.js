b5f3bf211b27db740787b2f601e80f2b71d2202a
Peter5793 <masaba.peter68@gmail.com>
2021-03-23 22:40:24 +0300

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends Rectangle {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-	    console.log(c.repeat(this.height));
+      console.log(c.repeat(this.height));
     }
   }
 }
