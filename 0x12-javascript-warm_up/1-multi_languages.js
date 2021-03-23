6d153f81893a31a6080efccf9039bb35787f083f
Peter5793 <masaba.peter68@gmail.com>
2021-03-23 08:30:35 +0300

    fix typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const prog = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const prog = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const i in prog) {
   console.log(prog[i]);
