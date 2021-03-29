99ff4c5ce4a2146d15d814c3624a57b6885b0618
Peter5793 <masaba.peter68@gmail.com>
2021-03-29 10:16:48 +0000

    use inner join for tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+list all the cities in the datase
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                ORDER BY cities.id ASC;")
+    states = cur.fetchall()
+
+    for x in states:
+        print(x)
+
+    cur.close()
+    db.close() 
