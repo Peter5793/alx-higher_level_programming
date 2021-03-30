240005a7ce5bdc73e386c394054bb52916fbe3cd
Peter5793 <masaba.peter68@gmail.com>
2021-03-30 22:05:04 +0300

    use join

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- list all the shows contained in hbtn_tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_shows_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER by tv_shows.title, tv_show_genres.genre_id;
