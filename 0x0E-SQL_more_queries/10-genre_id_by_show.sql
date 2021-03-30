95233ac404ed919e25a191a9d981c030fabe3a3b
Peter5793 <masaba.peter68@gmail.com>
2021-03-30 22:08:51 +0300

    fix the typo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list all the shows contained in hbtn_tv_shows
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-INNER JOIN tv_shows_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER by tv_shows.title, tv_show_genres.genre_id;
