e53d1efee600fa8b772f29a5600ba5416c56ba9b
Peter5793 <masaba.peter68@gmail.com>
2021-04-11 16:35:28 +0300

    edit query string

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a PPOST request to the passed URL
-curl -s -X POST -d "email=hr@holbertonschool.com&subjectI will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
