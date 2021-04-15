36aa2b8903e19eefac22811cd7955dfafc3d39b4
Peter5793 <masaba.peter68@gmail.com>
2021-04-15 16:13:56 +0300

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and displays the body of the response
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = request.get(url)
+    try:
+        r.raise_for_status()
+        print(r.text)
+    except Exception as e:
+        print("Error code: {}".format(r.status_code))
