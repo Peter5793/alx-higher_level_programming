1350f6220ab3ddc90098e860b286519751afe8b8
Peter5793 <masaba.peter68@gmail.com>
2021-04-15 12:28:41 +0300

    send POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes the URL  and an email sends a POST request to passed URL displays body
+"""
+from sys import argv
+import urllib.request
+
+if __name__ == '__main__':
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urrlib.request.Request(argv[1], data)
+    with urlib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8')
