3df1d98270ae96028edbdf3d937f215ff070ed8f
Peter5793 <masaba.peter68@gmail.com>
2021-04-15 15:47:13 +0300

    post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+takes the URL and email address and sends POST requests  to passed URL with
+email parameter and displays body of response
+"""
+from sys import argv
+import requets
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
