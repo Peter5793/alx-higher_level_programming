663683d474c734dca4341e21155c982730a90565
Peter5793 <masaba.peter68@gmail.com>
2021-04-15 12:33:05 +0300

    fix EOF

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     data = data.encode('utf-8')
     req = urrlib.request.Request(argv[1], data)
     with urlib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8')
+        print(response.read().decode('utf-8'))
