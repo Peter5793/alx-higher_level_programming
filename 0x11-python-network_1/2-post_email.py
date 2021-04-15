c8de3fbc7bfa32b7c8e63a8a64d71f6dcf912909
Peter5793 <masaba.peter68@gmail.com>
2021-04-15 12:41:45 +0300

    fix Pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,11 +4,12 @@ takes the URL  and an email sends a POST request to passed URL displays body
 """
 from sys import argv
 import urllib.request
+import urllib.parse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
-    req = urrlib.request.Request(argv[1], data)
-    with urlib.request.urlopen(req) as response:
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
