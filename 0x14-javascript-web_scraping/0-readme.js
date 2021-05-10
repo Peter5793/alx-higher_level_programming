9f6ad4034e11d840f1df6bf311d9fa25609d571b
Peter5793 <masaba.peter68@gmail.com>
2021-05-10 13:30:51 +0300

    change argument

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const argv = process.argv;
 const fs = require('fs');
+const argv = process.argv[2];
 
-fs.readFile(argv[2], 'utf-8', (err, data) => {
+fs.readFile(argv, 'utf-8', function(err, data) {
   if (err) {
     console.log(err);
   } else {
