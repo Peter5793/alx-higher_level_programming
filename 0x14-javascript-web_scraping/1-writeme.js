aa11c273d4f881a43f56ac682ef1e4f284372e5b
Peter5793 <masaba.peter68@gmail.com>
2021-05-10 13:39:50 +0300

    semi-standard error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 const fs = require('fs');
 
-fs.writeFile(argv[2], argv[3], 'utf-8', function(err, data) {
+fs.writeFile(argv[2], argv[3], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   }
