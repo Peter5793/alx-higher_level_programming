18c40e992c4768495c39e073e21bb32c44c60044
Peter5793 <masaba.peter68@gmail.com>
2021-05-10 13:36:17 +0300

    readfile

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const argv = process.argv;
 const fs = require('fs');
 
-fs.writeFile(argv[2], argv[3], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	}
+fs.writeFile(argv[2], argv[3], 'utf-8', function(err, data) {
+  if (err) {
+    console.log(err);
+  }
 });
