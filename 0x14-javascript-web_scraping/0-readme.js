d2678cf017dc6224a9565135418e435daacd1550
Peter5793 <masaba.peter68@gmail.com>
2021-05-10 13:41:59 +0300

    fix semi-standard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const argv = process.argv[2];
 
-fs.readFile(argv, 'utf-8', function(err, data) {
+fs.readFile(argv, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
