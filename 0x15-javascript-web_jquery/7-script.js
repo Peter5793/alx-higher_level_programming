a4b738035e643758ae8b3826cb7b48c89e89fa83
Peter5793 <masaba.peter68@gmail.com>
2021-05-15 12:24:31 +0300

    use ajax method

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+$(function () {
+	$.ajax ({
+		url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+		type: 'GET',
+		success: function(data) {
+			$('#character').text(data.name);
+		}
+	});
+});
+
