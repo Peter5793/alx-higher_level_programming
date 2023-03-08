bc45972415292150baf4205c7b11c1546aca179d
Peter Lougerlier <masaba.peter68@gmail.com>
2023-03-08 20:20:22 +0100

    Create 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- a script that lists all shows, and all genres linked to that show
+
+SELECT a.title, c.name FROM tv_shows AS a 
+LEFT JOIN tv_show_genres AS b  
+ON a.id = b.show_id 
+LEFT JOIN tv_genres AS c 
+ON c.id = b.genre_id 
+ORDER BY a.title ASC;
