ae07d589653574bea884bb99bd24079c6cae108d
Peter Lougerlier <masaba.peter68@gmail.com>
2023-03-08 11:22:34 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
 -- write script to convert database to utf8mb
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
