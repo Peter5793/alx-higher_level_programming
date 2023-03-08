bc9407de50e0dc3af6acc3a12983b54e22fdaf67
Peter Lougerlier <masaba.peter68@gmail.com>
2023-03-08 23:43:52 +0100

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,7 @@
 -- shows tv title by their ratings
-SELECT a.title, sum(c.rate) AS rating FROM tv_shows AS a 
-  INNER JOIN tv_show_genres AS b
-    ON a.id = b.show_id 
-  INNER JOIN tv_show_ratings AS c
-    ON b.show_id = c.show_id 
-  GROUP BY a.title 
-  ORDER BY rating DESC;
+SELECT a.title, sum(b.rate) AS rating FROM tv_shows AS a 
+INNER JOIN tv_show_ratings AS b 
+ON a.id = b.show_id 
+GROUP BY a.title 
+ORDER BY rating DESC;
+
