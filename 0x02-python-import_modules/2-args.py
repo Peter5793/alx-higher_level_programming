4475443df925b9c9a5378939dee51d0037ae910c
Peter Lougerlier <masaba.peter68@gmail.com>
2023-03-12 12:49:44 +0100

    Update 2-args.py
    remove hyphen

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv)
     if x <= 1:
-        print("0-arguments.")
+        print("0 arguments.")
     else:
         if x == 2:
             print("{} argument:".format(x - 1))
