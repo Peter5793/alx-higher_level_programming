e77e515a3139b5cfd482c1a7e820aa8b21b7d3d5
Peter Lougerlier <masaba.peter68@gmail.com>
2023-09-12 14:01:00 +0200

    Update 0-add_integer.py
    account for NaN

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,12 @@ def add_integer(a, b=98):
     """
     return the sum of two integers
     """
-    
+    if a is None:
+        raise TypeError('a must be an integer')
+    if b is None:
+        raise TypeError('b must be an integer')
     if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError ('a must be an integer')
+        raise TypeError('a must be an integer')
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
     else:
